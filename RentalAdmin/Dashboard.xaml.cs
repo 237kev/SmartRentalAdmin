b2b913f4a21eb62d45d2d506aa62c3cc00f68086
Kevin <k3v1n.k4md3m@gmail.com>
2025-06-28 23:28:24 +0200

    first commit

---
+++ RentalAdmin/Dashboard.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data;
+using Npgsql;
+
+namespace RentalAdmin   
+{
+    /// <summary>
+    /// Interaction logic for Dashboard.xaml
+    /// </summary>
+    public partial class Dashboard : Window
+    {
+        //Host=localhost
+        //Username = postgres
+        //Password= **********
+        //Database = databaseLegrandpre
+
+        // le string de connection
+        private string connectionString = "Host=localhost;Username=postgres;Password=123456789;Database=databaseLegrandpre";
+        public Dashboard()
+        {
+            InitializeComponent();
+            LoadHebergement();
+        }
+
+        private void LoadHebergement()
+        {
+            using (var npgsqlConnection = new NpgsqlConnection(connectionString))
+            {
+                //ouverture de la connection
+                npgsqlConnection.Open();
+                // TODO: rediger le query souhaité
+                string query = "SELECT ID_hebergement, Nom, Type, Prix_par_nuit, Capacite, Description FROM Hebergements";
+                using (var npgsqlCmd = new NpgsqlCommand(query, npgsqlConnection))
+                using (var npgaqlAdapter = new NpgsqlDataAdapter(npgsqlCmd))
+                {
+                    // créer une table de donnée
+                    DataTable dt = new DataTable();
+                    //remplir la table de donnée
+                    npgaqlAdapter.Fill(dt);
+                    //afficher dans DataGrid
+                    HebergementsDataGrid.ItemsSource = dt.DefaultView;
+                }
+            }
+        }
+
+
+    }
+}
---
+++ RentalAdmin/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RentalAdmin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string identifiantAdmin = UsernameTextBox.Text;
+            string motDePasse = PasswordBox.Password;
+
+            try
+            {
+                if (identifiantAdmin == "admin" && motDePasse == "1234")
+                {
+                   // il est question de creer une instance du tableau de bord
+                   Dashboard dashboard = new Dashboard();
+                    //puis l'afficher et
+                    dashboard.Show();
+                    //ensuite fermer la fenetre de connection
+                    this.Close();
+
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Identifiant ou mot de passe incorrect");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+ 
+    }
+}
