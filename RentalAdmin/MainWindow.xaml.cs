f6512a9211e7b5e664dc0380ce3053ca566f1fc3
Kevin <k3v1n.k4md3m@gmail.com>
2025-07-09 21:00:08 +0200

    feat: rounded Border and gradient color blue and orange

--- RentalAdmin/MainWindow.xaml.cs
+++ RentalAdmin/MainWindow.xaml.cs
@@ -16,14 +16,17 @@ namespace RentalAdmin
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string CheminPhotoLogo = "C:\\Users\\Kevin\\Pictures\\appartement\\logo\\logo.png";
         public MainWindow()
         {
+            
             InitializeComponent();
+            this.DataContext = this;
         }
 
         private void LoginButton_Click(object sender, RoutedEventArgs e)
         {
-            
+
             string identifiantAdmin = UsernameTextBox.Text;
             string motDePasse = PasswordBox.Password;
 
@@ -31,14 +34,14 @@ namespace RentalAdmin
             {
                 if (identifiantAdmin == "admin" && motDePasse == "1234")
                 {
-                   // il est question de creer une instance du tableau de bord
-                   Dashboard dashboard = new Dashboard();
-                    //puis l'afficher et
+                    //                    il est question de creer une instance du tableau de bord
+                    Dashboard dashboard = new Dashboard();
+                    //                    puis l'afficher et
                     dashboard.Show();
-                    //ensuite fermer la fenetre de connection
+                    //                   ensuite fermer la fenetre de connection
                     this.Close();
 
-                    
+
                 }
                 else
                 {
@@ -49,9 +52,9 @@ namespace RentalAdmin
             {
                 MessageBox.Show(ex.Message);
             }
-            
+
         }
 
- 
+
     }
 }
